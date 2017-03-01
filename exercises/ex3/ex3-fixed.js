1428864a7b507dbe8825d16743277863f9c5a5b0
kbeloborodko <kbeloborodko@gmail.com>
2017-03-01 18:56:30 +0200

    Reset the initial version. Provide a solution.

--- exercises/ex3/ex3-fixed.js
+++ exercises/ex3/ex3-fixed.js
@@ -1,7 +1,11 @@
-function mult(x,y,z) {
-	return x * y * z;
+function mult(...args) {
+  if (args.length <= 2) {
+    return args[0] * args[1];
+  }
+
+  return args[0] * mult(...args.slice(1));
 }
 
-mult(3,4,5);	// 60
+console.log(mult(3,4,5));// 60
 
-mult(3,4,5,6);	// Oops!
+console.log(mult(3,4,5,6));  // Oops!
