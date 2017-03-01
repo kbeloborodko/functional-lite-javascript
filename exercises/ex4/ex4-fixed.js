b07aa1c6eeb602ae96eee573e5ff84ab709443ff
kbeloborodko <kbeloborodko@gmail.com>
2017-03-01 19:58:32 +0200

    First two points solution

--- exercises/ex4/ex4-fixed.js
+++ exercises/ex4/ex4-fixed.js
@@ -1,1 +1,14 @@
-// code here! :)
+function returnRandomNumber1 () {
+  return Math.floor(Math.random() * 10);
+}
+
+function returnRandomNumber2 () {
+  return Math.floor(Math.random() * 10);
+}
+
+function add (x, y) {
+  return x + y;
+}
+
+console.log('Sum of the random numbers is: ' + add(returnRandomNumber1(), returnRandomNumber2()));
+
