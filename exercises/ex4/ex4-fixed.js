22af1662c79e9295434e75476ec5c073601eb052
kbeloborodko <kbeloborodko@gmail.com>
2017-03-01 20:20:35 +0200

    Solution for 4 and 7, 5, 6 in progress

--- exercises/ex4/ex4-fixed.js
+++ exercises/ex4/ex4-fixed.js
@@ -1,20 +1,49 @@
-function returnRandomNumber1 () {
-  return Math.floor(Math.random() * 10);
-}
+(function () {
+  'use strict';
 
-function returnRandomNumber2 () {
-  return Math.floor(Math.random() * 10);
-}
+  function returnRandomNumber1 () {
+    return Math.floor(Math.random() * 10);
+  }
 
-function add (x, y) {
-  return x + y;
-}
+  function returnRandomNumber2 () {
+    return Math.floor(Math.random() * 10);
+  }
 
-console.log('Sum of the random numbers is: ' + add(returnRandomNumber1(), returnRandomNumber2()));
+  function add (x, y) {
+    return x + y;
+  }
 
-function add2 (func1, func2) {
-  return add(func1(), func2());
-}
+  console.log('Sum of the random numbers is: ' + add(returnRandomNumber1(), returnRandomNumber2()));
 
-console.log('Sum of the random numbers using add2 function is: ' + add(returnRandomNumber1(), returnRandomNumber2()));
+  function add2 (func1, func2) {
+    return add(func1(), func2());
+  }
 
+  console.log('Sum of the random numbers using add2 function is: ' + add(returnRandomNumber1(), returnRandomNumber2()));
+
+  function returnRandomFunction () {
+    return function () {
+      return Math.floor(Math.random() * 10);
+    }
+  }
+
+  var randomNumberFunc = returnRandomFunction();
+
+  console.log('A random number using the returnRandomFunction utility: ' + randomNumberFunc());
+
+  function addn (...args) {
+    args.reduce(function (prev, current) {
+      console.log(prev, current);
+      return add2(prev, current);
+    });
+  }
+
+  console.log('A sum of 3 random numbers using addn function: ' + addn(returnRandomNumber1, returnRandomNumber2));
+
+  var evenOddArray = [1, 2, 2, 3, 2, 4, 2, 5, 6, 7];
+  var filteredEvenOddArray = evenOddArray.filter(function (item) {
+    return item % 2 === 0;
+  });
+
+  console.log('Filter [1, 2, 2, 3, 2, 4, 2, 5, 6, 7] array to contain only even numbers', filteredEvenOddArray);
+})();
